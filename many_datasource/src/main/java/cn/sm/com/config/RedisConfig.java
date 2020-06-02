a3aad78a03b3d81d1ab47bcb949eaeca32026875
xubo <527461024@qq.com>
2020-06-02 14:05:50 +0800

    1404公司第三次

--- many_datasource/src/main/java/cn/sm/com/config/RedisConfig.java
+++ many_datasource/src/main/java/cn/sm/com/config/RedisConfig.java
@@ -1,10 +1,8 @@
 package cn.sm.com.config;
-
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
